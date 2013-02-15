3fd8de73f29697ec06b243e3d641ab5fb3848763
paulghahn2 <paulghahn2@gmail.com>
2013-02-14 18:03:13 -0800

    Completed Chapter 12 Iteration G1

---
+++ app/models/order.rb
@@ -0,0 +1,16 @@
+class Order < ActiveRecord::Base
+
+  has_many :line_items, :dependent => :destroy
+  
+  PAYMENT_TYPES = ["Check", "Credit Card", "Purchase Order"]
+  validates_format_of :email, :with => /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
+  validates :name, :address, :email, :pay_type, :presence => true
+  validates :pay_type, :inclusion => PAYMENT_TYPES
+  
+  def add_line_items_from_cart(cart)
+  	cart.line_items.each do |item|
+  		item.cart_id = nil
+  		line_items << item
+   end
+  end	
+end
