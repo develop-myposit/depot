0f04656ff936ff1904d388bfd152266bd02b2af0
Paul Hahn <paulghahn2@gmail.com>
2013-03-09 21:16:19 -0800

    completed H1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,9 +13,23 @@ Depot::Application.configure do
   config.consider_all_requests_local       = true
   config.action_view.debug_rjs             = true
   config.action_controller.perform_caching = false
-
+  #
+  # Mailer Settings
+  #
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
+  # Use SMTP & configure it
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port    => 587,
+    :domain  => "dwz.mygbiz.com",
+    :authentication => "plain",
+    :user_name => "dog",
+    :password => "foofoobarbar",
+    :enable_starttls_auto => true
+  }
+
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
